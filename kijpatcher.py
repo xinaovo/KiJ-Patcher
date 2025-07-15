65b061ce25d0e5ebf5bcc9e4926d05e5ce089c59
xinaovo <xinaqwq@outlook.com>
2025-07-15 12:26:26 +0800

    Updated variable names.

--- kijpatcher.py
+++ kijpatcher.py
@@ -21,16 +21,16 @@ import zipfile
 import datetime
 import argparse
 
-# 下单用的文件的位置
-path_final = "patched"
+# Output path of patched files
+patchedFilesPath = "patched"
 
 # Gerber files filter
-file_filter = ('.gbl','.gbs','.gbp','.gbo','.gm1','gm13',
+fileFilter = ('.gbl','.gbs','.gbp','.gbo','.gm1','gm13',
                '.gtl','.gts','.gtp','.gto','.drl','.G1',
                '.G2','.gko')
 
 # EasyEDA version string
-jlcEditorVersion = "6.5.50"
+easyedaVersionString = "6.5.50"
 
 # Generate header with current time
 gerberHeader="""G04 Layer: BottomLayer*
@@ -38,15 +38,15 @@ G04 EasyEDA v{}, {}*
 G04 Gerber Generator version 0.2*
 G04 Scale: 100 percent, Rotated: No, Reflected: No *
 G04 Dimensions in inches *
-G04 leading zeros omitted , absolute positions ,3 integer and 6 decimal *""".format(jlcEditorVersion, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+G04 leading zeros omitted , absolute positions ,3 integer and 6 decimal *""".format(easyedaVersionString, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
-jlc_order_tips_txt="""如何进行PCB下单
+jlcOrderTipsText="""如何进行PCB下单
 
 请查看：
 https://docs.lceda.cn/cn/PCB/Order-PCB"""
 
 # Replace list of file suffix and file name.
-replace_list_end = [('.gbl',"Gerber_BottomLayer.GBL", "BottomLayer"),
+replaceListFileSuffix = [('.gbl',"Gerber_BottomLayer.GBL", "BottomLayer"),
                     ('.gko',"Gerber_BoardOutlineLayer.GKO", "BoardOutlineLayer"),
                     ('.gbp',"Gerber_BottomPasteMaskLayer.GBP",),
                     ('.gbo',"Gerber_BottomSilkscreenLayer.GBO", "BottomSilkscreenLayer"),
@@ -59,7 +59,7 @@ replace_list_end = [('.gbl',"Gerber_BottomLayer.GBL", "BottomLayer"),
                     ('.gm1',"Gerber_MechanicalLayer1.GM1"),
                     ('.gm13',"Gerber_MechanicalLayer13.GM13")]
 
-replace_list_contain = [('_PCB-PTH', "Drill_PTH_Through.DRL"),
+replaceListFileName = [('_PCB-PTH', "Drill_PTH_Through.DRL"),
                         ('_PCB-NPTH', "Drill_NPTH_Through.DRL"),
                         ('-PTH', "Drill_PTH_Through.DRL"),
                         ('-NPTH', "Drill_NPTH_Through.DRL"),
@@ -67,63 +67,63 @@ replace_list_contain = [('_PCB-PTH', "Drill_PTH_Through.DRL"),
                         ('_PCB-In2_Cu', "Gerber_InnerLayer2.G2"),
                         ('_PCB-Edge_Cuts', "Gerber_BoardOutlineLayer.GKO")]
 
-def zipFolder(folder_path, output_path):
+def zipFolder(folderPath, outputPath):
     """
     Compress a folder
-    :param folder_path: Path to input folder
-    :param output_path: Path to output .zip file.
+    :param folderPath: Path to input folder
+    :param outputPath: Path to output .zip file.
     """
-    with zipfile.ZipFile(output_path, "w", zipfile.ZIP_DEFLATED) as zip:
-        for root, dirs, files in os.walk(folder_path):
+    with zipfile.ZipFile(outputPath, "w", zipfile.ZIP_DEFLATED) as zip:
+        for root, dirs, files in os.walk(folderPath):
             for file in files:
                 file_path = os.path.join(root, file)
-                zip.write(file_path, os.path.relpath(file_path, folder_path))
+                zip.write(file_path, os.path.relpath(file_path, folderPath))
 
 # Read Gerber and drill file, add JLC-specific header and write it to output dir with corresponding name.
-def patchSingleFile(filename, path_out):
+def patchSingleFile(filename, outputPath):
     # Read file by line
     lines = open(filename).readlines()
 
     # Rename file with name corresponding to the filetype and add JLC specific header
-    hit_flag = 0
+    flag = 0
     currentLayer = ""
 
-    for replace_couple in replace_list_end:
-        if filename.endswith(replace_couple[0]):
-            file_new = open(path_out + '/' + replace_couple[1], 'w')
-            hit_flag = 1
+    for fileSuffixPair in replaceListFileSuffix:
+        if filename.endswith(fileSuffixPair[0]):
+            newFile = open(outputPath + '/' + fileSuffixPair[1], 'w')
+            flag = 1
             break
 
-    if hit_flag == 0:
-        for replace_couple in replace_list_contain:
-            if filename.find(replace_couple[0]) != -1:
-                file_new = open(path_out + '/' + replace_couple[1], 'w')
-                hit_flag = 1
+    if flag == 0:
+        for fileNamePair in replaceListFileName:
+            if filename.find(fileNamePair[0]) != -1:
+                newFile = open(outputPath + '/' + fileNamePair[1], 'w')
+                flag = 1
                 break
 
-    if hit_flag == 1:
-        hit_flag = 0
+    if flag == 1:
+        flag = 0
 
-        file_new.write(gerberHeader)
+        newFile.write(gerberHeader)
 
         for line in lines:
-            file_new.write(line)
+            newFile.write(line)
 
-        file_new.close()
+        newFile.close()
 
-def pathInit(path_out):
+def pathInit(outputPath):
     # Create output directory if it doesn't exist
-    folder_out = os.path.exists(path_out)
-    if not folder_out:
-        print("Directory %s not found, creating now..." % path_out)
-        os.makedirs(path_out)
+    outputFolder = os.path.exists(outputPath)
+    if not outputFolder:
+        print("Directory %s not found, creating now..." % outputPath)
+        os.makedirs(outputPath)
     else:
-        print("Directory \"%s\" exists. Skipping..." % path_out)
+        print("Directory \"%s\" exists. Skipping..." % outputPath)
 
     # Empty the directory
     print("Directory is not empty. Deleting everything...")
-    for files in os.listdir(path_out):
-        path = os.path.join(path_out, files)
+    for files in os.listdir(outputPath):
+        path = os.path.join(outputPath, files)
         try:
             shutil.rmtree(path)
         except OSError:
@@ -144,19 +144,19 @@ if __name__ == "__main__":
     os.chdir(gerberFilesDir)
     pathInit("patched")
 
-    file_count = 0
+    fileCount = 0
     fileList = os.listdir(gerberFilesDir)
 
     # Iterate files in the gerber dir and patch them/.
     for p in fileList:
         if(os.path.isfile(os.path.join(gerberFilesDir, p))):
-            if(p.endswith(file_filter)):
+            if(p.endswith(fileFilter)):
                 print("Gerber file %s found, patching..." % p)
-                patchSingleFile(os.path.join(gerberFilesDir, p), os.path.join(os.getcwd(), path_final))
-                file_count += 1
+                patchSingleFile(os.path.join(gerberFilesDir, p), os.path.join(os.getcwd(), patchedFilesPath))
+                fileCount += 1
 
-    with open(gerberFilesDir + "/" + path_final + "/PCB下单必读.txt", "w") as tipstxt:
-        tipstxt.write(jlc_order_tips_txt)
+    with open(gerberFilesDir + "/" + patchedFilesPath + "/PCB下单必读.txt", "w") as tipstxt:
+        tipstxt.write(jlcOrderTipsText)
     
     timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
 
@@ -166,5 +166,5 @@ if __name__ == "__main__":
     else:
         outputFilePath = args.output_file
 
-    zipFolder(path_final , outputFilePath)
+    zipFolder(patchedFilesPath , outputFilePath)
     print("Patched Gerber files saved as", outputFilePath)
