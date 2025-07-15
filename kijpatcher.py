feaf91f557fe16f625d9d593b88dd574f89515d9
xinaovo <xinaqwq@outlook.com>
2025-07-15 17:34:15 +0800

    New features: Add corresponding layer identifier to files; Add random gerber id to files.

--- kijpatcher.py
+++ kijpatcher.py
@@ -20,7 +20,9 @@ import shutil
 import zipfile
 import datetime
 import argparse
+import random
 
+PROGRAM_VERSION_STRING = "V0.99 dev"
 # Output path of patched files
 patchedFilesPath = "patched"
 
@@ -32,40 +34,35 @@ fileFilter = ('.gbl','.gbs','.gbp','.gbo','.gm1','gm13',
 # EasyEDA version string
 easyedaVersionString = "6.5.50"
 
-# Generate header with current time
-gerberHeader="""G04 Layer: BottomLayer*
-G04 EasyEDA v{}, {}*
-G04 Gerber Generator version 0.2*
-G04 Scale: 100 percent, Rotated: No, Reflected: No *
-G04 Dimensions in inches *
-G04 leading zeros omitted , absolute positions ,3 integer and 6 decimal *""".format(easyedaVersionString, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-
+# Order tips text
 jlcOrderTipsText="""如何进行PCB下单
 
 请查看：
 https://docs.lceda.cn/cn/PCB/Order-PCB"""
 
+RANDOM_ID_LENGTH = 32
+
 # Replace list of file suffix and file name.
 replaceListFileSuffix = [('.gbl',"Gerber_BottomLayer.GBL", "BottomLayer"),
                     ('.gko',"Gerber_BoardOutlineLayer.GKO", "BoardOutlineLayer"),
-                    ('.gbp',"Gerber_BottomPasteMaskLayer.GBP",),
+                    ('.gbp',"Gerber_BottomPasteMaskLayer.GBP", "BottomPasteMaskLayer"),
                     ('.gbo',"Gerber_BottomSilkscreenLayer.GBO", "BottomSilkscreenLayer"),
                     ('.gbs',"Gerber_BottomSolderMaskLayer.GBS", "BottomSolderMaskLayer"),
                     ('.gtl',"Gerber_TopLayer.GTL", "TopLayer"),
                     ('.gtp',"Gerber_TopPasteMaskLayer.GTP", "TopPasteMaskLayer"),
                     ('.gto',"Gerber_TopSilkscreenLayer.GTO", "TopSilkscreenLayer"),
                     ('.gts',"Gerber_TopSolderMaskLayer.GTS", "TopSolderMaskLayer"),
-                    ('.gd1',"Drill_Through.GD1"),
-                    ('.gm1',"Gerber_MechanicalLayer1.GM1"),
-                    ('.gm13',"Gerber_MechanicalLayer13.GM13")]
+                    ('.gd1',"Drill_Through.GD1", ""),
+                    ('.gm1',"Gerber_MechanicalLayer1.GM1", ""),
+                    ('.gm13',"Gerber_MechanicalLayer13.GM13", "")]
 
-replaceListFileName = [('_PCB-PTH', "Drill_PTH_Through.DRL"),
-                        ('_PCB-NPTH', "Drill_NPTH_Through.DRL"),
-                        ('-PTH', "Drill_PTH_Through.DRL"),
-                        ('-NPTH', "Drill_NPTH_Through.DRL"),
-                        ('_PCB-In1_Cu', "Gerber_InnerLayer1.G1"),
-                        ('_PCB-In2_Cu', "Gerber_InnerLayer2.G2"),
-                        ('_PCB-Edge_Cuts', "Gerber_BoardOutlineLayer.GKO")]
+replaceListFileName = [('_PCB-PTH', "Drill_PTH_Through.DRL", ""),
+                        ('_PCB-NPTH', "Drill_NPTH_Through.DRL", ""),
+                        ('-PTH', "Drill_PTH_Through.DRL", ""),
+                        ('-NPTH', "Drill_NPTH_Through.DRL", ""),
+                        ('_PCB-In1_Cu', "Gerber_InnerLayer1.G1", "InnerLayer1"),
+                        ('_PCB-In2_Cu', "Gerber_InnerLayer2.G2", "InnerLayer2"),
+                        ('_PCB-Edge_Cuts', "Gerber_BoardOutlineLayer.GKO", "BoardOutlineLayer")]
 
 def zipFolder(folderPath, outputPath):
     """
@@ -79,8 +76,28 @@ def zipFolder(folderPath, outputPath):
                 file_path = os.path.join(root, file)
                 zip.write(file_path, os.path.relpath(file_path, folderPath))
 
+def generateRandomString(length: int):
+    ALPHABETS = "abcdef0123456789"
+    result = ""
+    for i in range(length):
+        index = random.randint(0, len(ALPHABETS) - 1)
+        result += ALPHABETS[index]
+    return result
+
+def getGerberHeader(layer, versionString, timestamp, id1, id2):
+    gerberHeader="""G04 Layer: {}*
+G04 EasyEDA v{}, {}*
+G04 {},{},10*
+G04 Gerber Generator version 0.2*
+G04 Scale: 100 percent, Rotated: No, Reflected: No *
+G04 Dimensions in inches *
+G04 leading zeros omitted , absolute positions ,3 integer and 6 decimal *""".format(layer,
+                                                                                     versionString, timestamp.strftime("%Y-%m-%d %H:%M:%S"),
+                                                                                       id1, id2)
+    return gerberHeader
+
 # Read Gerber and drill file, add JLC-specific header and write it to output dir with corresponding name.
-def patchSingleFile(filename, outputPath):
+def patchSingleFile(filename, outputPath, id1, id2):
     # Read file by line
     lines = open(filename).readlines()
 
@@ -91,6 +108,7 @@ def patchSingleFile(filename, outputPath):
     for fileSuffixPair in replaceListFileSuffix:
         if filename.endswith(fileSuffixPair[0]):
             newFile = open(outputPath + '/' + fileSuffixPair[1], 'w')
+            currentLayer = fileSuffixPair[2]
             flag = 1
             break
 
@@ -98,13 +116,18 @@ def patchSingleFile(filename, outputPath):
         for fileNamePair in replaceListFileName:
             if filename.find(fileNamePair[0]) != -1:
                 newFile = open(outputPath + '/' + fileNamePair[1], 'w')
+                currentLayer = fileNamePair[2]
                 flag = 1
                 break
 
     if flag == 1:
         flag = 0
 
-        newFile.write(gerberHeader)
+        # If the corresponding value of board layer identifier is missing, use default value "BottomLayer"
+        if len(currentLayer) == 0:
+            currentLayer = "BottomLayer"
+
+        newFile.write(getGerberHeader(currentLayer,easyedaVersionString, datetime.datetime.now(), id1, id2))
 
         for line in lines:
             newFile.write(line)
@@ -132,6 +155,11 @@ def pathInit(outputPath):
 # Program Entry
 if __name__ == "__main__":
     # Command line options parser init.
+    print("""KiJ Patcher {}
+Copyright (c) 2024-2025 Xina.
+Copyright (c) 2023 ngHackerX86.
+This is a free software released under GNU GPLv2. See LICENSE for more information.
+""".format(PROGRAM_VERSION_STRING))
     parser = argparse.ArgumentParser(prog="KiJ Patcher",
                                      usage="kijpatcher -i <input> -o <output>",
                                      description="Patch KiCad generated gerber file to complies with JLC rules.",
@@ -148,11 +176,13 @@ if __name__ == "__main__":
     fileList = os.listdir(gerberFilesDir)
 
     # Iterate files in the gerber dir and patch them/.
+    randomID1 = generateRandomString(RANDOM_ID_LENGTH)
+    randomID2 = generateRandomString(RANDOM_ID_LENGTH)
     for p in fileList:
         if(os.path.isfile(os.path.join(gerberFilesDir, p))):
             if(p.endswith(fileFilter)):
                 print("Gerber file %s found, patching..." % p)
-                patchSingleFile(os.path.join(gerberFilesDir, p), os.path.join(os.getcwd(), patchedFilesPath))
+                patchSingleFile(os.path.join(gerberFilesDir, p), os.path.join(os.getcwd(), patchedFilesPath), randomID1, randomID2)
                 fileCount += 1
 
     with open(gerberFilesDir + "/" + patchedFilesPath + "/PCB下单必读.txt", "w") as tipstxt:
